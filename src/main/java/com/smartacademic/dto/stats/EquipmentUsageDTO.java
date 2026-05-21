f5cefcdf0eec6c53838faa6e77aed3dc25079d33
Nguyen Quang Anh <youremail@example.com>
2026-05-21 20:10:22 +0700

    Update lecturer

--- src/main/java/com/smartacademic/dto/stats/EquipmentUsageDTO.java
+++ src/main/java/com/smartacademic/dto/stats/EquipmentUsageDTO.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Top thiết bị được mượn nhiều nhất.
- */
+
+ //thiết bị được mượn nhiều nhất.
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
